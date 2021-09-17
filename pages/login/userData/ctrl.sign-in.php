3b40a81fe31e4cc170ff53466efd2716ea4c31fe
Gerald Funtanar <geraldfuntanar@gmail.com>
2021-09-17 14:14:12 +0800

    addmision added

--- pages/login/userData/ctrl.sign-in.php
+++ pages/login/userData/ctrl.sign-in.php
@@ -7,20 +7,55 @@ if (isset($_POST['signin'])) {
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
 
-    $super_admin = mysqli_query($db, "SELECT * FROM tbl_super_admins WHERE username = '$username' ");
-    $numrow1 = mysqli_num_rows($super_admin);
+    $super_admin = mysqli_query($db, "SELECT * FROM tbl_super_admins WHERE username = '$username'");
+    $numrow = mysqli_num_rows($super_admin);
 
-    if ($numrow1 > 0) {
+    $dean = mysqli_query($db, "SELECT * FROM tbl_deans WHERE username = '$username'");
+    $numrow1 = mysqli_num_rows($dean);
+
+    $admission = mysqli_query($db, "SELECT * FROM tbl_admissions WHERE username = '$username'");
+    $numrow2 = mysqli_num_rows($admission);
+
+
+
+    if ($numrow > 0) {
         while ($row = mysqli_fetch_array($super_admin)) {
-            $hashedPwdCheck1 = password_verify($password, $row['password']);
-            if ($hashedPwdCheck1 == false) {
-                echo "<script>alert('Your password is invalid!'); window.location='signin.php'</script>";
+            $hashedPwdCheck = password_verify($password, $row['password']);
+            if ($hashedPwdCheck == false) {
+                header("location: ../sign-in.php?sessionP");
                 exit();
-            } elseif ($hashedPwdCheck1 == true) {
+            } elseif ($hashedPwdCheck == true) {
                 $_SESSION['role'] = "Super Administrator";
                 $_SESSION['userid'] = $row['sa_id'];
             }
             header("location: ../../dashboard/dashboard.php");
         }
+    } elseif ($numrow1 > 0) {
+        while ($row = mysqli_fetch_array($dean)) {
+            $hashedPwdCheck = password_verify($password, $row['password']);
+            if ($hashedPwdCheck == false) {
+                header("location: ../sign-in.php?sessionP");
+                exit();
+            } elseif ($hashedPwdCheck == true) {
+                $_SESSION['role'] = "Dean";
+                $_SESSION['userid'] = $row['dean_id'];
+            }
+            header("location: ../../dashboard/dashboard.php");
+        }
+    }elseif ($numrow2 > 0) {
+        while ($row = mysqli_fetch_array($admission)) {
+            $hashedPwdCheck = password_verify($password, $row['password']);
+            if ($hashedPwdCheck == false) {
+                header("location: ../sign-in.php?sessionP");
+                exit();
+            } elseif ($hashedPwdCheck == true) {
+                $_SESSION['role'] = "Admission";
+                $_SESSION['userid'] = $row['admission_id'];
+            }
+            header("location: ../../dashboard/dashboard.php");
+        }
+    }  else {
+        header("location: ../sign-in.php?sessionUP");
+        exit();
     }
 }
