4ecbe640a0ad391d5ddda8cfdb367ebdcbef6b68
Jason Casas <jason.carolino.casas@gmail.com>
2021-12-29 09:05:11 +0800

    edit and added connection string for online conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -4,4 +4,9 @@ $username = "root";
 $password = "";
 $dbname = "enrollmentlp";
 
+// $servername = "localhost";
+// $username = "u733437513_josh";
+// $password = "9JM!gh]9K";
+// $dbname = "u733437513_enrollmentlp";
+
 $db = new mysqli($servername, $username, $password, $dbname) or die($db->error);
