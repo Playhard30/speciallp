95ebd73cd7205bf283049beb98c56eddc9d98cdd
Josh <josheugenio0909@gmail.com>
2022-01-16 13:58:57 +0800

    class_code (Backup)

--- pages/subject/userData/ctrl.edit.subject.php
+++ pages/subject/userData/ctrl.edit.subject.php
@@ -21,7 +21,7 @@ if (isset($_POST['submit'])) {
     $checkSubj = mysqli_query($db, "SELECT * , tbl_courses.course_id FROM tbl_subjects_new LEFT JOIN tbl_courses ON tbl_courses.course_id = tbl_subjects_new.course_id WHERE subj_code = '$subj_code' AND subj_desc = '$subj_desc' AND tbl_subjects_new.course_id = '$course' AND eay_id = '$eay_id'") or die(mysqli_error($db));
     $resultCheck = mysqli_num_rows($checkSubj);
 
-    // if subject is exist get num_rows
+    // if subject is exist get num_rows.
     $get_classCode = $db->query("SELECT * FROM tbl_schedules WHERE subj_id = '$subj_id'");
     $result_classCode = $get_classCode->num_rows;
 
