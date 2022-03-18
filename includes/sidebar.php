cf828a3c67c47ccf4b708bf75b001d3c99611f79
jasonkid0 <jason.carolino.casas@gmail.com>
2022-03-18 11:57:18 +0800

    put comment
    jdfksdjhfdsf

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,4 +1,5 @@
 <aside
+<!--   this is just a comment      -->
     class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 shadow-xl shadow bg-white"
     data-color="danger" id="sidenav-main">
     <div class="sidenav-header">
