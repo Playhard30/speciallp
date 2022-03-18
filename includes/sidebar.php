d87bb9a173f41348982afff2b852c7113e85be4c
Jason Casas <jason.carolino.casas@gmail.com>
2022-03-18 12:02:55 +0800

    ayaw ko ng comment

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,5 @@
 <aside
-<!--   this is just a comment      -->
+<!--   this is just a comment  ayaw ko na comment    -->
     class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 shadow-xl shadow bg-white"
     data-color="danger" id="sidenav-main">
     <div class="sidenav-header">
